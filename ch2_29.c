9814f107563a3ee65913068566418a6429fc9dbb
Can ÖZKAN <72666934+can-ozkan@users.noreply.github.com>
2020-12-30 00:26:56 +0300

    Add files via upload

---
+++ ch2_29.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    // A B C a b c 0 1 2 $ * + /
+    printf("A is %d\n", 'A');
+    printf("B is %d\n", 'B');
+    printf("C is %d\n", 'C');
+    printf("a is %d\n", 'a');
+    printf("b is %d\n", 'b');
+    printf("0 is %d\n", '0');
+    printf("1 is %d\n", '1');
+    printf("2 is %d\n", '2');
+    printf("$ is %d\n", '$');
+    printf("* is %d\n", '*');
+    printf("+ is %d\n", '+');
+    printf("/ is %d\n", '/');
+
+    return 0;
+}
