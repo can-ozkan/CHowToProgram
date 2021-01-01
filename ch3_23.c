0c2fd548f310b5c2f6a42943e5e874258a83fb8c
Can ÖZKAN <72666934+can-ozkan@users.noreply.github.com>
2021-01-01 12:24:29 +0300

    Add files via upload

---
+++ ch3_23.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    unsigned int counter=0, number, largest=0;
+
+    puts("Find the Largest Number Application");
+
+    while (counter < 10)
+    {
+        printf("Please, enter a value: ");
+        scanf("%u", &number);
+
+        if (number > largest)
+        {
+            largest = number;
+        }
+        
+        counter++;
+    }
+    
+    printf("The largest value is %d\n", largest);
+    
+    return 0;
+}
