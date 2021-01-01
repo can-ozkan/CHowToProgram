2eb430faa54cdba702b13b3cf37927a40c567559
Can ÖZKAN <72666934+can-ozkan@users.noreply.github.com>
2021-01-01 14:52:54 +0300

    Add files via upload

---
+++ ch3_40.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int i=1, counter;
+
+    puts("Powers of 3 with an Infinite Loop");
+
+/*
+    while (1)
+    {
+        i *= 3;
+        printf("%d\n", i);
+    }
+
+*/
+
+    for (counter = 0; counter < 10; counter++)
+    {
+        i *= 3;
+        printf("%d\n", i);
+    }
+    
+    
+    
+    return 0;
+}
