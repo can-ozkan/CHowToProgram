46a2f5766abfbb334f8e8ff7caf60ba57752710c
Can ÖZKAN <72666934+can-ozkan@users.noreply.github.com>
2021-03-05 13:14:16 +0300

    Add files via upload

---
+++ reversing_array_pointer.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#define number 5
+
+int main(int argc, char const *argv[])
+{
+    int a[number] = {1,2,3,4,5};
+    int *p = a;
+    int i;
+
+    for ( p = a+number-1; p >=a ; p--)
+    {
+        printf("%d\n", *p);
+    }
+    
+    return 0;
+}
